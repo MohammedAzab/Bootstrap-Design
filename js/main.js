f00a148be93eea808f63f66783fb8152d9d05c03
Mohamed Azab <mohamedazab.info@gmail.com>
2017-08-02 14:33:41 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+  // navbar
+  $('.navbar-nav>li').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+
+  // color Option function
+  $('.icon').click(function(){
+    $(this).toggleClass('change');
+    if ($(this).hasClass('change')) {
+      $('.option-box').fadeIn();
+    }
+    if (!$(this).hasClass('change')) {
+      $('.option-box').fadeOut();
+    }
+  })
+
+  $('.color-o li').click(function(){
+    $("link[href*='theme']").attr('href', $(this).attr('data-value'));
+  });
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() >= 700) {
+      $('.top').fadeIn();
+    }else {
+      $('.top').fadeOut();
+    }
+  });
+  $('.top').click(function() {
+    $('html body').animate({scrollTop: 0}, 600);
+  });
+
+  if ($(document).ready()) {
+    $('.loading-cover').delay(5000).fadeOut();
+  }
+});
